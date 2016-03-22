d09adcee06dcfd8181a48e57d37629ab9f812cc5
Christophe Porteneuve <tdd@tddsworld.com>
2016-03-22 19:44:44 +0100

    Étape 3 : layout, contrôleur et middleware à nous

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 import { createServer } from 'http'
 import express from 'express'
+import { join as joinPaths } from 'path'
 import logger from 'morgan'
 
 import 'colors'
 
+import mainController from './controllers/main'
+
 const app = express()
 const server = createServer(app)
+const publicPath = joinPaths(__dirname, 'public')
 
 app.set('port', process.env.PORT || 3000)
 app.set('view engine', 'jade')
 
+app.use(express.static(publicPath))
 app.use(logger(app.get('env') === 'development' ? 'dev' : 'combined'))
 
 app.locals.title = 'Wazaaa'
@@ -18,7 +23,13 @@ if (app.get('env') === 'development') {
   app.locals.pretty = true
 }
 
-app.get('/', (req, res) => res.render('home'))
+app.use((req, res, next) => {
+  res.locals.flash = []
+  res.locals.url = req.url
+  next()
+})
+
+app.use(mainController)
 
 server.listen(app.get('port'), () => {
   console.log('✔ Server listening on port'.green, String(app.get('port')).cyan)
